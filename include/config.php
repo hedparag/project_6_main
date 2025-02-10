52d66b1f97afd8dff09b22bf83df715322e20dcf
Baishakhi Sharma <baishakhisharma214@gmail.com>
2025-02-10 13:37:07 +0530

    test

---
+++ include/config.php
@@ -0,0 +1,16 @@
+<?php
+$host = "localhost"; 
+$port = "5432";      
+$dbname = "my_db"; 
+$user = "postgres";        
+$password = "1234";    
+
+try {
+    $dsn = "pgsql:host=$host;port=$port;dbname=$dbname";
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+    
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
