2c48a4e93b75566b6ba6ae1570e035c3688829d5
Baishakhi Sharma <baishakhisharma214@gmail.com>
2025-02-19 13:14:22 +0530

    logout

--- logout.php
+++ logout.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Logout - Employee Management System</title>
-    <link rel="stylesheet" href="logoutcss.css">
+    <link rel="stylesheet" href="css/logoutcss.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
